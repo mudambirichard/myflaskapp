c1705df8a005c1e7b83e31f8ec83a020432a8644
mudambi richard <mudarichards@gmail.com>
2018-05-04 15:46:55 +0300

    modify files

---
+++ data.py
@@ -0,0 +1,30 @@
+def Articles():
+	articles = [
+           {
+           'id': 1,
+           'title':'Articles One',
+           'body': 'Andela extends engineering teams with the most talented software developers in Africa. We recruit the most talented technologists on the African continent, shape them into technical leaders, and place them as full-time distributed team members with companies that range from Microsoft and IBM to dozens of high-growth ...'
+           'author''Muda Richs',
+           'create_date': '04-05-2017'
+            },
+
+           {
+            'id': 2,
+            'title':'Articles Two',
+            'body': 'Andela extends engineering teams with the most talented software developers in Africa. We recruit the most talented technologists on the African continent, shape them into technical leaders, and place them as full-time distributed team members with companies that range from Microsoft and IBM to dozens of high-growth ...'
+            'author' 'Musa Musa',
+            'create_date': '04-05-2017'
+           
+
+           },
+
+           {
+            'id': 3,
+           'title':'Articles Thre e',
+           'body': 'Andela extends engineering teams with the most talented software developers in Africa. We recruit the most talented technologists on the African continent, shape them into technical leaders, and place them as full-time distributed team members with companies that range from Microsoft and IBM to dozens of high-growth ...'
+           'author' 'Karts Agrey',
+           'create_date': '04-05-2017'
+           }
+
+	]
+	return articles
