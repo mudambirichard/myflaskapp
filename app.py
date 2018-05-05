b7c0af2242316b9b7cbfe26b21a59ba6ee584a97
mudambi richard <mudarichards@gmail.com>
2018-05-05 23:59:04 +0300

    modify app.py

--- app.py
+++ app.py
@@ -3,10 +3,36 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
-
+from functools import wraps
 app = Flask(__name__)
 
+#config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'THEGREAT'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#init MYSQL
+mysql = MySQL(app)
+
 Articles = Articles()
+
+
+#check if user logout in
+def is_logged_in(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('Unauthorised, Please login', 'danger')
+			return redirect(url_for('login'))
+	return wrap
+
+
+@app.route('/')
+def index():
+	return render_template('home.html')
 
 @app.route('/home')
 def home():
@@ -39,9 +65,81 @@ class RegisterForm(Form):
 def register():
 	form = RegisterForm(request.form)
 	if request.method == 'POST' and form.validate():
-		return render_template('register.html')
+		name = form.name.data
+		email = form.email.data
+		username = form.username.data
+		password = sha256_crypt.encrypt(str(form.password.data))
+
+		#create cursor
+		cur = mysql.connection.cursor()
+
+		cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+		#commit to db
+		mysql.connection.commit()
+
+		#close connection
+		cur.close()
+
+		flash('You are now registered', 'Success')
+
+		return redirect(url_for('login'))
         return render_template('register.html', form=form)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+
+		#GET FORM
+		username = request.form['username']
+		password_candidate = request.form['password']
+
+		#create cursor 
+		cur = mysql.connection.cursor()
+
+		# get user by username
+		result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+		if result > 0:
+
+			data = cur.fetchone()
+			password = data['password']
+
+			if sha256_crypt.verify(password_candidate, password):
+			   #passed
+			   session['logged_in'] = True
+			   session['username'] = username
+
+			   flash('You are now logged in','success')
+			   return redirect(url_for('dashboard'))
+
+			else:
+				error = 'Invalid login'
+	    	return render_template('login.html',error=error)
+            #close conection
+                cur.close()
+
+	       # else:
+                error = 'Username not found'
+	    	return render_template('login.html',error=error)
+
+        return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+	session.clear()
+	flash('You are now logged out', 'success')
+	return redirect(url_for('login'))
+	
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+ 	return render_template('dashboard.html')
+ 
+
 if __name__ == '__main__':
+	app.secret_key='secret123'
 	app.run(debug=True)
